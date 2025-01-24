e69723613958a2a89276289a5390446a03869e65
Caleb Habesh <87959618+calebhabesh@users.noreply.github.com>
2025-01-24 11:18:20 -0500

    Used DICOM importer and different smoothing algorithms on TCIA dataset
    Getting issues with converting image file format plus 2D/3D issues
    however.

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,45 @@
+#include "FAST/Data/DataTypes.hpp"
+#include <FAST/Algorithms/GaussianSmoothing/GaussianSmoothing.hpp>
+#include <FAST/Algorithms/ImageCaster/ImageCaster.hpp>
+#include <FAST/Algorithms/ImageSharpening/ImageSharpening.hpp>
 #include <FAST/Algorithms/NonLocalMeans/NonLocalMeans.hpp>
-#include <FAST/Importers/ImageFileImporter.hpp>
+#include <FAST/Data/DataTypes.hpp>
+#include <FAST/Importers/DICOMFileImporter.hpp>
 #include <FAST/Visualization/DualViewWindow.hpp>
-#include <FAST/Visualization/ImageRenderer/ImageRenderer.hpp>
 #include <FAST/Visualization/SimpleWindow.hpp>
+#include <FAST/Visualization/SliceRenderer/SliceRenderer.hpp>
+// #include <FAST/Visualization/ImageRenderer/ImageRenderer.hpp>
 
 using namespace fast;
 
 int main(int argc, char **argv) {
-  auto importer = ImageFileImporter::create(
-      Config::getTestDataPath() + "US/Heart/ApicalFourChamber/US-2D_0.mhd");
+  // Define the DICOM File Importer and point to T1C Brain Tumor Dataset .dcm
+  // images
+  auto importer = DICOMFileImporter::create(
+      Config::getTestDataPath() +
+      "Brain-Tumor-Progression/PGBM-017/09-17-1997-RA FH MR RCBV "
+      "OP-85753/16.000000-T1post-19554/1-14.dcm");
 
-  // Set up the NonLocalMeans processing step and connect it to the importer
+  // Set up the NonLocalMeans processing step and connect it to
+  // the DICOM importer, currently has issues with data type
   auto filter = NonLocalMeans::create()->connect(importer);
 
-  // Set a renderer and connect it to importer
-  auto renderer = ImageRenderer::create()->connect(importer);
+  // GaussianSmoothing Class
+  auto smoothing = GaussianSmoothing::create(2, 3)->connect(importer);
 
-  // Set a renderer and connect it to the NonLocalMeans filter
-  auto filterRenderer = ImageRenderer::create()->connect(filter);
+  // Extract a 2D slice from the original 3D image
+  auto originalSliceRenderer =
+      SliceRenderer::create(PlaneType::PLANE_Z, -1)->connect(importer);
+
+  // Extract a 2D slice from the filtered image
+  auto filteredSliceRenderer =
+      SliceRenderer::create(PlaneType::PLANE_Z, -1)->connect(smoothing);
 
   // Create a dual view window, add the renderers and start the
   // computation/rendering loop.
   DualViewWindow2D::create()
-      ->connectLeft(renderer)
-      ->connectRight(filterRenderer)
+      ->connectLeft(originalSliceRenderer)  // un-filtered image
+      ->connectRight(filteredSliceRenderer) // filtered-image
       ->run();
   return 0;
 }
